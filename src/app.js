c9467d0dc963ffc87b712da693382de96a92f4a0
Sourav Routh <67389838+SouravR7@users.noreply.github.com>
2020-12-15 01:11:23 +0530

    changg

--- src/app.js
+++ src/app.js
@@ -4,24 +4,48 @@ const app = express()
 
 
 // Your code goes here
+const youtubeSubscriber = require('./models/subscribers');
 
+app.use(express.json());
+app.use(bodyParser.json());
 
+app.get("/subscribers", (req,res)=>{
 
+    youtubeSubscriber.find({}, (err, data) => {
+        if(err){
+            res.status(400).json({"message": error.message});
+        }
+        else{
+            res.json(data);
+        }
+    });
+})
 
+app.get("/subscribers/:id", (req,res)=>{
 
+    youtubeSubscriber.findOne({
+        _id: id,
+    }).then((err,data) => {
+        if (err) {
+            res.status(400).json({"message": error.message});
+        } else{
+            res.json(data);
+        }
+    });
+});
 
+app.get("/subscribers/:names", (req,res)=>{
 
-
-
-
-
-
-
-
-
-
-
-
+    youtubeSubscriber.findOne({
+        _name: name,
+    }).then((err,data) => {
+        if (err) {
+            res.status(400).json({"message": error.message});
+        } else{
+            res.json(data);
+        }
+    });
+});
 
 
 
